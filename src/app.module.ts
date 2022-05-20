e9ee67b4ed153137f3c88a2a1534d2963e17cb81
Julio Costa <barbosacostajulio@gmail.com>
2022-05-20 01:00:44 -0300

    improves structure

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,31 +6,15 @@ import {
 } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { typeOrmAsyncConfig } from 'config/database/typeorm.config';
-//import { typeOrmAsyncConfig } from 'src/config/typeorm.config';
-import { QuizModule } from './modules/quiz/quiz.module';
-import { UserModule } from './modules/user/user.module';
-import { AuthModule } from './modules/auth/auth.module';
-import { ApiTokenCheckMiddleware } from './common/middleware/api-token-check.middleware';
 import { EventEmitterModule } from '@nestjs/event-emitter';
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
     TypeOrmModule.forRootAsync(typeOrmAsyncConfig),
     EventEmitterModule.forRoot(),
-    QuizModule,
-    UserModule,
-
   ],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
 })
-export class AppModule implements NestModule {
-  configure(consumer: MiddlewareConsumer) {
-    consumer
-      .apply(ApiTokenCheckMiddleware)
-      .forRoutes({ path: '/', method: RequestMethod.ALL });
-  }
-}
+export class AppModule {}
---
+++ src/interface/http/controllers/_controller.module.ts
@@ -0,0 +1,8 @@
+import { Module } from '@nestjs/common';
+import { ServicesModule } from 'src/app/services/_service.module';
+@Module({
+  imports: [],
+  providers: [],
+  controllers: [],
+})
+export class ControllersModule { }
